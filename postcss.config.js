f2cbf4e3f9ef365693330b818b3331c5f12c3fc7
Lauri Eskola <lauri.eskola@acquia.com>
2019-10-01 18:32:12 +0300

    Issue #3084189 by lauriii, fhaeberle, huzooka: Import variables.css manually

--- postcss.config.js
+++ postcss.config.js
@@ -3,17 +3,14 @@ const postcssCustomProperties = require('postcss-custom-properties');
 module.exports = ctx => ({
   map: !ctx.env || ctx.env !== 'production' ? { inline: false } : false,
   plugins: [
+    require('postcss-import'),
     require('postcss-custom-properties')({
       preserve: false,
-      // Breaks style lint and unnecessary if preserve set to false.
-      // exportTo: 'dist-css/variables.css',
-      importFrom: [
-        './css/src/base/variables.css'
-      ]
     }),
-    require("postcss-calc"),
+    require('postcss-calc'),
     require('autoprefixer')({
       cascade: false
-    })
+    }),
+    require('postcss-discard-comments')
   ]
 });
