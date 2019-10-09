5ca1c3ecb410978b2d6003051e540bf869984d74
Lauri Eskola <lauri.eskola@acquia.com>
2019-10-09 19:31:09 +0300

    Issue #3086510 by huzooka, lauriii, ckrina, saschaeggi: Proposal for (status) message spacing

--- js/messages.es6.js
+++ js/messages.es6.js
@@ -3,20 +3,62 @@
  * Message template overrides.
  */
 
-Drupal.theme.message = ({ text }, { type, id }) => {
-  const messagesTypes = Drupal.Message.getMessageTypeLabels();
-  const messageWrapper = document.createElement('div');
+((Drupal) => {
+  /**
+   * Override Drupal.Message.defaultWrapper() because it prevents adding classes
+   * to the wrapper.
+   *
+   *  @return {HTMLElement}
+   *   The default destination for JavaScript messages.
+   *
+   * @todo Revisit this after https://www.drupal.org/node/3086723 has been
+   *   resolved.
+   */
+  Drupal.Message.defaultWrapper = () => {
+    let wrapper = document.querySelector('[data-drupal-messages]');
+    if (!wrapper) {
+      wrapper = document.querySelector('[data-drupal-messages-fallback]');
+      wrapper.removeAttribute('data-drupal-messages-fallback');
+      wrapper.setAttribute('data-drupal-messages', '');
+      wrapper.classList.remove('hidden');
+      wrapper.classList.add('messages-list');
+    }
+    return wrapper.innerHTML === ''
+      ? Drupal.Message.messageInternalWrapper(wrapper)
+      : wrapper.firstElementChild;
+  };
 
-  messageWrapper.setAttribute('class', `messages messages--${type}`);
-  messageWrapper.setAttribute(
-    'role',
-    type === 'error' || type === 'warning' ? 'alert' : 'status',
-  );
-  messageWrapper.setAttribute('aria-labelledby', `${id}-title`);
-  messageWrapper.setAttribute('data-drupal-message-id', id);
-  messageWrapper.setAttribute('data-drupal-message-type', type);
+  /**
+   * Overrides message theme function.
+   *
+   * @param {object} message
+   *   The message object.
+   * @param {string} message.text
+   *   The message text.
+   * @param {object} options
+   *   The message context.
+   * @param {string} options.type
+   *   The message type.
+   * @param {string} options.id
+   *   ID of the message, for reference.
+   *
+   * @return {HTMLElement}
+   *   A DOM Node.
+   */
+  Drupal.theme.message = ({ text }, { type, id }) => {
+    const messagesTypes = Drupal.Message.getMessageTypeLabels();
+    const messageWrapper = document.createElement('div');
 
-  messageWrapper.innerHTML = `
+    messageWrapper.setAttribute('class', `messages messages--${type}`);
+    messageWrapper.setAttribute(
+      'role',
+      type === 'error' || type === 'warning' ? 'alert' : 'status',
+    );
+    messageWrapper.setAttribute('aria-labelledby', `${id}-title`);
+    messageWrapper.setAttribute('data-drupal-message-id', id);
+    messageWrapper.setAttribute('data-drupal-message-type', type);
+
+    messageWrapper.innerHTML = `
     <div class="messages__header">
       <h2 id="${id}-title" class="messages__title">
         ${messagesTypes[type]}
@@ -27,5 +69,6 @@ Drupal.theme.message = ({ text }, { type, id }) => {
     </div>
   `;
 
-  return messageWrapper;
-};
+    return messageWrapper;
+  };
+})(Drupal);
--- js/messages.js
+++ js/messages.js
@@ -5,21 +5,35 @@
 * @preserve
 **/
 
-Drupal.theme.message = function (_ref, _ref2) {
-  var text = _ref.text;
-  var type = _ref2.type,
-      id = _ref2.id;
+(function (Drupal) {
+  Drupal.Message.defaultWrapper = function () {
+    var wrapper = document.querySelector('[data-drupal-messages]');
+    if (!wrapper) {
+      wrapper = document.querySelector('[data-drupal-messages-fallback]');
+      wrapper.removeAttribute('data-drupal-messages-fallback');
+      wrapper.setAttribute('data-drupal-messages', '');
+      wrapper.classList.remove('hidden');
+      wrapper.classList.add('messages-list');
+    }
+    return wrapper.innerHTML === '' ? Drupal.Message.messageInternalWrapper(wrapper) : wrapper.firstElementChild;
+  };
 
-  var messagesTypes = Drupal.Message.getMessageTypeLabels();
-  var messageWrapper = document.createElement('div');
+  Drupal.theme.message = function (_ref, _ref2) {
+    var text = _ref.text;
+    var type = _ref2.type,
+        id = _ref2.id;
 
-  messageWrapper.setAttribute('class', 'messages messages--' + type);
-  messageWrapper.setAttribute('role', type === 'error' || type === 'warning' ? 'alert' : 'status');
-  messageWrapper.setAttribute('aria-labelledby', id + '-title');
-  messageWrapper.setAttribute('data-drupal-message-id', id);
-  messageWrapper.setAttribute('data-drupal-message-type', type);
+    var messagesTypes = Drupal.Message.getMessageTypeLabels();
+    var messageWrapper = document.createElement('div');
 
-  messageWrapper.innerHTML = '\n    <div class="messages__header">\n      <h2 id="' + id + '-title" class="messages__title">\n        ' + messagesTypes[type] + '\n      </h2>\n    </div>\n    <div class="messages__content">\n      ' + text + '\n    </div>\n  ';
+    messageWrapper.setAttribute('class', 'messages messages--' + type);
+    messageWrapper.setAttribute('role', type === 'error' || type === 'warning' ? 'alert' : 'status');
+    messageWrapper.setAttribute('aria-labelledby', id + '-title');
+    messageWrapper.setAttribute('data-drupal-message-id', id);
+    messageWrapper.setAttribute('data-drupal-message-type', type);
 
-  return messageWrapper;
-};
+    messageWrapper.innerHTML = '\n    <div class="messages__header">\n      <h2 id="' + id + '-title" class="messages__title">\n        ' + messagesTypes[type] + '\n      </h2>\n    </div>\n    <div class="messages__content">\n      ' + text + '\n    </div>\n  ';
+
+    return messageWrapper;
+  };
+})(Drupal);
